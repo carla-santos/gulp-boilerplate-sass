e40b9133e6e78d270889eec66a1a8f54ea71a85a
bakum-bicum <carla.devjs@gmail.com>
2022-09-15 00:55:04 -0300

    Adiciona arquivos .js de teste

---
+++ src/js/area.js
@@ -0,0 +1,6 @@
+function areaQuadrado(lado) {
+  const area = lado * 4;
+  return area;
+}
+
+console.log(areaQuadrado(2));
---
+++ src/js/media.js
@@ -0,0 +1,6 @@
+function media(nota1, nota2) {
+  const media = (nota1 + nota2) / 2;
+  return media;
+}
+
+console.log(media(6, 8));
